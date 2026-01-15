acf5d8aa6ba7a64cef8641f20f75ce074aab21d0
natively.dev <api@natively.dev>
2026-01-15 03:17:51 +0000

    style: Use primary brand green for "Locate Here" button

--- app/(tabs)/(home)/getting-to-studio.tsx
+++ app/(tabs)/(home)/getting-to-studio.tsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   button: {
-    backgroundColor: '#2D5016',
+    backgroundColor: '#111F0F',
     paddingVertical: 12,
     paddingHorizontal: 20,
     borderRadius: 8,
